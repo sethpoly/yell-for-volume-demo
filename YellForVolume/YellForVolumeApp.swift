90e59d4efeb9ccf425dbfbc2bfd9d10a8eb40035
Seth Polyniak <emailspolyniak@gmail.com>
2022-12-07 18:31:34 -0500

    [main] - first commit

---
+++ YellForVolume/YellForVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellForVolumeApp.swift
+//  YellForVolume
+//
+//  Created by Seth Polyniak on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct YellForVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
